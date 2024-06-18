9cb2bac7c772a7e58c323081ceca574ad924d841
leonard adjei <157623061+leonardeganow@users.noreply.github.com>
2024-06-18 09:03:48 +0000

    updated contact numbers in footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -129,12 +129,16 @@ const Footer = (props) => {
                 <div className="contact-ft">
                   <ul>
                     <li>
-                      <i className="fi ti-location-pin"></i>Cantonment - Accra
-                      <br /> P. O. Box CT 1763
+                      <i className="fi ti-location-pin"></i>No.C10/12 Adama
+                      Avenue, Adabraka, {/* <br /> P. O. Box CT 1763 */}
                     </li>
                     <li>
                       <i className="fi flaticon-phone-call-1"></i>+233 (30)
-                      2223195
+                      2222222
+                      <br />
+                      +(233) 509823400
+                      <br />
+                      +(233) 531033503
                     </li>
                     <li>
                       <i className="fa fa-envelope-o" aria-hidden="true"></i>
